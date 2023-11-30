b78a7bfb713e57a453ede58f7e9345feeea76dfb
Vinicius Diniz <viniciusdiniz-ieg@instituto.ad>
2023-11-30 05:56:05 -0300

    Refatorando conexão ws

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
+            var ws = new WebSocket("ws://backend-hacktoon.onrender.com/ws");
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
