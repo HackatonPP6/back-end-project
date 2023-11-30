eae38ce9ffc7a8b77dba099add02c74d411da5b8
Vinicius Diniz <viniciusdiniz-ieg@instituto.ad>
2023-11-30 06:30:04 -0300

    Alterando conexao ws para cloud

---
+++ src/service/aws.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from src.domain.logs import Logs
+
+class AwsService:
+    def get_AWS_log():
+        navegador = webdriver.Chrome()
+        aws_status = []
+
+        tem_degradation_sp = False
+        tem_degradation_vi = False
+    
+        try:
+            navegador.get('https://health.aws.amazon.com/health/status')
+    
+            time.sleep(2)
+            filtro = navegador.find_element(By.ID, 'status-history-property-filter').find_elements(By.XPATH, './/*')[10]
+    
+            filtro.send_keys("N. Virginia")
+            filtro.send_keys(Keys.DOWN)
+            filtro.send_keys(Keys.ENTER)
+    
+            filtro.send_keys("Sao Paulo")
+            filtro.send_keys(Keys.DOWN)
+            filtro.send_keys(Keys.ENTER)
+            time.sleep(2)
+    
+            btn_aumentar = navegador.find_element(By.CLASS_NAME, "table-tray").find_element(By.TAG_NAME, "a")
+    
+            navegador.execute_script("arguments[0].scrollIntoView();", btn_aumentar)
+            btn_aumentar.click()
+    
+            time.sleep(1)
+            linhas = navegador.find_elements(By.TAG_NAME, 'tr')[2:]
+    
+            for linha in linhas:
+                n_nome = linha.find_elements(By.XPATH, './/*')[3].get_attribute("innerHTML")
+                n_status = linha.find_elements(By.XPATH, './/*')[15].get_attribute("aria-label").strip()
+            
+                if "Virginia" in n_nome:
+                    n_nome = n_nome.split("(")[0] + "(N. Virginia)"
+                    aws_status.append(Logs(n_nome, n_status, "AWS.Virginia"))
+                else:
+                    n_nome = n_nome.split("(")[0] + "(Sao Paulo)"
+                    aws_status.append(Logs(n_nome, n_status, "AWS.SaoPaulo"))
+        finally:
+            navegador.quit()
+    
+        return aws_status
+    
+    print(get_AWS_log())
